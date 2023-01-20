2d452c0173a018d5ba07402d4a56035aaaab9e2c
ahmedezz012 <ahmed_ezz2021995@yahoo.com>
2023-01-20 15:15:12 +0200

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Ahmed Ezz on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
